1a9f2fd30f907d9f141c03871c2cb4610fa55f31
mtzfernando <mtz_fernando@yahoo.com>
2018-09-17 13:50:42 -0600

    Added piping capability and updated the Readme

--- shell/Readme.md
+++ shell/Readme.md
@@ -1,8 +1,9 @@
 # Shell in python3
 
 This directory contains the code for a shell written in python3. The shell 
-has the capabilities of running basic shell commands. Also it has the 
-capability of redirecting input and output. 
+has the capabilities of running basic shell commands. Also, it has the
+capability of redirecting input and output. Added capability to be able to
+pipe the output of one program into the input of another.
 
 ### To run
 `$ python3 myShell.py`
--- shell/myShell.py
+++ shell/myShell.py
@@ -7,12 +7,39 @@ import os
 import re
 import sys
 
+def pipe(arg):                                          # Function to take care of the piping
+    read, write = os.pipe()                             # Create the read and write pipe
+    fc = os.fork()                                      # Fork a child off the child
+    if fc == 0:
+        os.close(1)                                     # Redirect to the pipe's output
+        write = os.dup(write)
+        os.set_inheritable(write, True)                 # Set it to inheritable
+        for dir in re.split(":", os.environ['PATH']):   # try each directory in the path
+            program = "%s/%s" % (dir, args[0])
+            try:
+                os.execve(program, (arg[0], arg[1]), os.environ)    # try to exec program
+            except FileNotFoundError:                               # ...expected
+                pass
+    else:
+        os.close(0)                                             # Redirect to the pipe's input
+        read = os.dup(read)
+        os.set_inheritable(read, True)                          # Set it to inheritable
+        os.wait()                                               # Wait for child to finish
+        for dir in re.split(":", os.environ['PATH']):           # try each directory in the path
+            program = "%s/%s" % (dir, args[2])
+            try:
+                os.execle(program, arg[2], os.environ)          # try to exec program
+            except FileNotFoundError:                           # ...expected
+                pass
+
 pid = os.getpid()
 while 1:
     os.write(1, "My Shell $ ".encode())             # Print My shell
     command = os.read(0, 100).decode()              # Wait for command to execute
     args = command.split()                          # Split the command into a list
 
+    if len(args) == 0:
+        continue
     if args[0].lower() == 'exit':                   # If user types exit end the program
         sys.exit(0)
 
@@ -23,6 +50,9 @@ while 1:
         sys.exit(1)
 
     elif rc == 0:                                   # child
+        if '|' in args:                             # Check if it is a pipe
+            args.remove('|')                        # Remove the pipe from the list
+            pipe(args)                              # Call pipe function
         if '>' in args:
             os.close(1)                             # redirect child's stdout
             sys.stdout = open(args.pop(), "w")
@@ -39,12 +69,12 @@ while 1:
         for dir in re.split(":", os.environ['PATH']):  # try each directory in the path
             program = "%s/%s" % (dir, args[0])
             try:
-                os.execve(program, args, os.environ)  # try to exec program
+                os.execve(program, args, os.environ)   # try to exec program
             except FileNotFoundError:  # ...expected
                 pass
 
         os.write(2, ("Child:    Could not exec %s\n" % args[0]).encode())
-        sys.exit(1)                                 # terminate with error
+        sys.exit(1)                                    # terminate with error
 
-    else:                                           # parent (forked ok)
+    else:                                              # parent (forked ok)
         childPidCode = os.wait()
